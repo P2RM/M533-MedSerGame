d447e28cee02c2236cbee0da50f8fed6831cfdd1
Blancardo135 <blanchardromain02@gmail.com>
2025-05-17 16:13:28 +0200

    J'ai codé worldMap et Location

--- src/main/Location.java
+++ src/main/Location.java
@@ -1,5 +1,46 @@
 package main;
 
 public class Location {
+    private String nom;
+    private String descr;
+    private boolean etat;
+    private int positionX;
+    private int positionY;
+
+    public Location(String nom, String descr, boolean etat, int positionX, int positionY){
+        this.nom=nom;
+        this.descr=descr;
+        this.etat=etat;
+        this.positionX=positionX;
+        this.positionY=positionY;
+    }
     
+    public String getNom(){
+        return this.nom;
+    }
+
+    public String getDescr(){
+        return this.descr;
+    }
+
+    public boolean getEtat(){
+        return this.etat;
+    }
+
+    public int getPositionX(){
+        return this.positionX;
+    }
+
+    public int getPositionY(){
+        return this.positionY;
+    }
+
+    public void lockLocation(){
+        etat=true;
+    }
+
+    public void unlockLocation(){
+        etat=false;
+    }
+
 }
