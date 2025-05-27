eb6d6741c5e455be87ad984a206080fde0fc6a03
Blancardo135 <blanchardromain02@gmail.com>
2025-05-27 15:25:48 +0200

    g juste changé la taille de la map

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
     public static void main(String[] args) {
 
-        WorldMap map = new WorldMap(5, 5);
+        WorldMap map = new WorldMap(10, 10);
         Player player = new Player("Romain");
         CommandRegistry commandRegistry = new CommandRegistry();
         Game game = new Game(map, player, commandRegistry);
